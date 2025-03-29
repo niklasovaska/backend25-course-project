1f653df1736f806b394b501b4b0bba21d16df961
niklasovaska <niklas.ovaska@gmail.com>
2025-03-29 21:56:23 +0200

    first commit

---
+++ src/test/java/fi/haagahelia/courseproject/CourseprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.haagahelia.courseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
