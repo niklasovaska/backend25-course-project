921d033d68ea64fe2b5b96983184b870b9df9029
niklasovaska <niklas.ovaska@gmail.com>
2025-04-02 02:12:17 +0300

    add and save artist endpoints

--- src/main/java/fi/haagahelia/courseproject/web/AlbumController.java
+++ src/main/java/fi/haagahelia/courseproject/web/AlbumController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import fi.haagahelia.courseproject.domain.Album;
 import fi.haagahelia.courseproject.domain.AlbumRepository;
+import fi.haagahelia.courseproject.domain.Artist;
 import fi.haagahelia.courseproject.domain.ArtistRepository;
 import jakarta.validation.Valid;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
 
 
 
@@ -88,4 +93,26 @@ public class AlbumController {
         return "redirect:../albumlist";
     }
 
+    // Add artist
+    @GetMapping("/addartist")
+    public String addArtist(Model model) {
+        model.addAttribute("artist", new Artist());
+        model.addAttribute("artists", artistRepository.findAll());
+        return "addartist";
+    }
+
+    // Save artist
+    @PostMapping("/saveartist")
+    public String saveArtist(@Valid Artist artist, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()) {
+            model.addAttribute("artist", artist);
+            model.addAttribute("artists", artistRepository.findAll());
+            return "addartist";
+        }
+
+        artistRepository.save(artist);
+        return "redirect:/albumlist";
+    }
+    
+    
 }
