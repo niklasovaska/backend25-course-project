aa05d565f3324e2c1262c583b2265f07398f5e22
niklasovaska <niklas.ovaska@gmail.com>
2025-04-12 22:08:48 +0300

    userdetails service impl

---
+++ src/main/java/fi/haagahelia/courseproject/service/UserDetailsServiceImpl.java
@@ -0,0 +1,37 @@
+package fi.haagahelia.courseproject.service;
+
+import java.util.Optional;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import fi.haagahelia.courseproject.domain.AppUser;
+import fi.haagahelia.courseproject.domain.AppUserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final AppUserRepository repository;
+
+    public UserDetailsServiceImpl(AppUserRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<AppUser> user = repository.findByUsername(username);
+
+        UserBuilder builder = null;
+        if(user.isPresent()) {
+            AppUser currentUser = user.get();
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(currentUser.getPasswordHash());
+            builder.roles(currentUser.getRole());
+        } else {
+            throw new UsernameNotFoundException("User not found.");
+        }
+
+        return builder.build();
+    }
+}
