ba0de286ba5f9a3427edbbeb1b7a1cf2173ff924
niklasovaska <niklas.ovaska@gmail.com>
2025-04-16 16:50:39 +0300

    auth filter, auth manager, cors config

--- src/main/java/fi/haagahelia/courseproject/SecurityConfig.java
+++ src/main/java/fi/haagahelia/courseproject/SecurityConfig.java
@@ -14,8 +14,11 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import fi.haagahelia.courseproject.service.UserDetailsServiceImpl;
+import fi.haagahelia.courseproject.AuthenticationFilter;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -25,9 +28,13 @@ import java.util.Arrays;
 @EnableWebSecurity
 public class SecurityConfig {
     private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationFilter authenticationFilter;
+    private final AuthEntryPoint exceptionHandler;
 
-    public SecurityConfig(UserDetailsServiceImpl userDetailsService) {
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, AuthenticationFilter authenticationFilter, AuthEntryPoint exceptionHandler) {
         this.userDetailsService = userDetailsService;
+        this.authenticationFilter = authenticationFilter;
+        this.exceptionHandler = exceptionHandler;
     }
 
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
@@ -41,35 +48,44 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
-    // @Bean
-    // public CorsConfigurationSource corsConfigurationSource() {
-    //     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    //     CorsConfiguration config = new CorsConfiguration();
-    //     config.setAllowedOrigins(Arrays.asList("*"));
-    //     config.setAllowedMethods(Arrays.asList("*"));
-    //     config.setAllowedHeaders(Arrays.asList("*"));
-    //     config.setAllowCredentials(false);
-    //     config.applyPermitDefaultValues();
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();                    
+    }
 
-    //     source.registerCorsConfiguration("/**", config);
-    //     return source;
-    // }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("*"));
+        config.setAllowedMethods(Arrays.asList("*"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(false);
+        config.applyPermitDefaultValues();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
-    // @Bean
-    // public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-    //     http.csrf((csrf) -> csrf.disable())
-    //         .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.disable()))
-    //         .cors(withDefaults())
-    //         .sessionManagement((sessioManagement) -> sessioManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-    //         .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll().anyRequest().authenticated())
-    //         .addFilterBefore(authenicationFilter, UsernamePasswordAuthenticationFilter.class)
-    //         .exceptionHandling((excepetionHandling) -> exceptionHandling.authenticationEntryPoint(exceptionHandler));
+        // http.csrf((csrf) -> csrf.disable())
+        //     .cors(withDefaults())
+        //     .authorizeHttpRequests((authorizeHttpRequests) -> 
+        //         authorizeHttpRequests.anyRequest().permitAll());
 
-    //     // http.csrf((csrf) -> csrf.disable()).cors(withDefaults())
-    //     //     .authorizeHttpRequests((authorizeHttpRequests) ->
-    //     //         authorizeHttpRequests.anyRequest().permitAll());
-                
-    //     return http.build();
-    // }
+        http.csrf((csrf) -> csrf.disable())
+            .cors(withDefaults())
+            .sessionManagement((sessionManagement) -> sessionManagement
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated())
+            .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling((exceptionHandling) -> exceptionHandling
+                .authenticationEntryPoint(exceptionHandler));
+        
+            return http.build();
+    }
+
 }
